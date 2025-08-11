d4abfbb376a6df9e70edc05461e123b10e768f45
GokdenizAkbal <akbalgokdeniz@gmail.com>
2025-08-11 16:41:05 +0300

    Photos are updated to high quality and card.php - style.css updated.

--- card.php
+++ card.php
@@ -2,26 +2,67 @@
     <h2>Recent News</h2>
     <div class="news-grid">
 
-    <article class="news-card large">
-        <img src="Images/Review.png" alt="UX Review Presentations">
-        <div class="news-content">
-            <p class="news-meta">
-                Olivia Rhye  • 1 January 2023
-            </p>
-        <h3>UX Review Presentation</h3>
-<p>How do you create compelling presentations that wow your colleagues and impress your managers?</p>
-<div class="news-tags">
-    <span class="design">Design</span>
-    <span class="research">Research</span>
-    <span class="presentation">Presentation</span>
+        <article class="news-card large">
+            <img src="Images/Review.png" alt="UX Review Presentations">
+            <div class="news-content">
+                <p class="news-meta">
+                    Olivia Rhye • 1 January 2023
+                </p>
+                <h3>UX Review Presentation</h3>
+                <p>How do you create compelling presentations that wow your colleagues and impress your managers?</p>
+                <div class="news-tags">
+                    <span class="design">Design</span>
+                    <span class="research">Research</span>
+                    <span class="presentation">Presentation</span>
 
-</div>
-        </div>
-    </article>
-
-
+                </div>
+            </div>
+        </article>
 
 
+        <article class="news-card small">
+            <img src="Images/Migration.png" alt="Migrating to Linear 101">
+            <div class="news-content">
+                <p class="news-meta">Phoenix Baker • 1 Jan 2023</p>
+                <h3>Migrating to Linear 101</h3>
+                <p>Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get
+                    started...</p>
+                <div class="news-tags">
+                    <span class="design">Design</span>
+                    <span class="research">Research</span>
+                </div>
+            </div>
+        </article>
+
+
+        <article class="news-card small">
+            <img src="Images/Apı.png" alt="Building your API Stack">
+            <div class="news-content">
+                <p class="news-meta">Lana Steiner • 1 Jan 2023</p>
+                <h3>Building your API Stack</h3>
+                <p>The rise of RESTful APIs has been met by a rise in tools for creating, testing, and managing
+                    them...</p>
+                <div class="news-tags">
+                    <span class="design">Design</span>
+                    <span class="research">Research</span>
+                </div>
+            </div>
+        </article>
+
+
+        <article class="news-card large">
+            <img src="Images/Climate.png" alt="Climate Endgame">
+            <div class="news-content">
+                <p class="news-meta">Olivia Rhye • 1 Jan 2023</p>
+                <h3>Grid system for better Design User Interface</h3>
+                <p>A grid system is a design tool used to arrange content on a webpage. It is a series of vertical and
+                    horizontal lines...</p>
+                <div class="news-tags">
+                    <span class="design">Design</span>
+                    <span class="interface">Interface</span>
+                </div>
+            </div>
+        </article>
 
 
     </div>
